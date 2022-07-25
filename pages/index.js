9446113c67913df5fcb826691eae70a6b0d088cc
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-07-25 19:01:05 +0100

    install tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-     hello
+      <h1>Products</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
     </div>
   );
 }
