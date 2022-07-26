3a4685d583406165e2d350ab7d6710404bb9879e
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-07-26 20:59:45 +0100

    create products container responsive layout

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
-
 import Card from "../components/Card";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <div className="min-h-[70vh]">
-
-      <h1>Products</h1>
-      <ul>
+      <div className="min-h-[70vh] mx-auto">
+        <h1>Products</h1>
+        <div className="gap-10 g-red-300 
+        
+        my-10 
+        grid 
+        md:grid-cols-2  lg:grid-cols-3
+         2xl:grid-cols-4 3xl:flex flex-wrap justify-center min-h-full space">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        {/* <ul>
         <li>Product 1</li>
         <li>Product 2</li>
         <li>Product 3</li>
         <Card/>
-      </ul>
+      </ul> */}
       </div>
     </Layout>
   );
